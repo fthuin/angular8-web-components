3ee6800ed63d4325a2cb1edc7966487342f4d3aa
Florian Thuin <florian.thuin@gmail.com>
2019-06-26 22:34:37 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { NgCardElementComponent } from './ng-card-element/ng-card-element.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    NgCardElementComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent],
+  entryComponents: [NgCardElementComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(NgCardElementComponent, {
+      injector: this.injector
+    });
+
+    customElements.define('facebook-card', el);
+  }
+}
